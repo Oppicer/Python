3a3e86034b8475f0eba3700ace0bb9110199e56e
Oppicer <kadenw2045@gmail.com>
2024-08-02 12:52:43 -0700

    created a random name generator for fun
    Just a scripts that prints a random name when you run it.

---
+++ random_name_generator.py
@@ -0,0 +1,9 @@
+import random
+# Variables for random name generator
+first_names = ["jake", "brian","john","bill","rick","Freak","trey","donald"]
+last_names = ["baker","walker","quantavious","Pork", "Bob", "nye"]
+first_nameIndex = random.randint(1,8)
+last_nameIndex = random.randint(1,6)
+print (first_names[first_nameIndex])
+print (last_names[last_nameIndex])
+       
